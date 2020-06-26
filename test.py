311501d4476bac51d30924afe8ebb9d51d392e9d
usephysics <rttan@edu.uwaterloo.ca>
2020-06-26 17:26:00 -0400

    scraper done

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ def get_URLS():
     URLs = []
     months = ["January", "February", "March", "April", "May", "June", 
     "July", "August", "September", "October", "November", "December"]
-    num_months = 1 # number of months in domain
+    num_months = 49 # number of months in domain
     active_month = ""   # check used to only return 1 URL per month
     while len(URLs) < num_months:
         page = requests.get(URL).text
@@ -28,7 +28,7 @@ def get_URLS():
                         idx = date.index(month[0]) # to add year as well as month
                         URLs.append(("https://en.wikipedia.org" + entry.findAll("a")[2]["href"], date[idx:]))
         URL = "https://en.wikipedia.org" + soup.find("a", {"class":"mw-nextlink"})["href"]
-        time.sleep(5) # wikipedia pls no ban
+        time.sleep(2) # wikipedia pls no ban
     return URLs
 
 # this function visits a given URL and returns a list containing the top 10 most disliked videos
@@ -41,11 +41,12 @@ def scrape_page(URL):
     videos = []
 
     for row in entries:
-        title = re.findall(r'"([^"]*)"', row.findAll()[1].text)[0]
+        title = re.findall(r'"([^"]*)"', row.findAll(["th", "td"])[1].text)[0]
+        if title == "YouTube Spotlight": title: "YouTube" # name change mid way through
         uploader = row.findAll(["th", "td"])[2].text
         dislikes = row.findAll(["th", "td"])[3].text
         # earlier versions of the page used thousands of dislikes as their unit
-        if float(dislikes) > 150: dislikes = float(dislikes) / 1000
+        if float(dislikes) > 150: dislikes = str(float(dislikes) / 1000)
         videos.append([title, uploader, dislikes])
     return(videos)
 
@@ -55,8 +56,9 @@ while(URLs):
     URL, month = URLs.pop()
     data.append([month, scrape_page(URL)])
 
-for month in data:
-    print("Month: " + month[0] + ". Song: " + month[1][0][0] + ". Dislikes (Millions): " + month[1][0][2])
+file = open("data.py", "a")
+file.write(str(data))
+file.close()
 
 # data[] --> Represents a month, ex: all data for June 2020
 # data[][0] --> The month as a string, ex: "June 2020"
